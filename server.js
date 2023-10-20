de30e4c9a7bf4dadc70b284f92224f43cd924bf5
ALBERT O MATARA <amatara@asu.edu>
2023-10-19 20:56:02 -0500

    started my Node Project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const hostname ='albert';
+const port = 3001;
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'))
+
+app.use((req,res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+
+});
